5ffd123d037e7eef6fcdb5bab2fa56cc2d2b5ca7
atif <khansmile646@gmail.com>
2023-05-22 20:31:53 +0530

    updated

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react'
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../pages/firebase'
 import { doc, setDoc } from "firebase/firestore";
+import { toast } from 'react-hot-toast';
 
 
 
 function Registration() {
 
     const [err, setErr] = useState(false)
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         const name = e.target[0].value;
@@ -18,12 +20,15 @@ function Registration() {
         try {
 
             const res = await createUserWithEmailAndPassword(auth, email, password)
-            await setDoc(doc(db, "users", "res.user.uid"), {
+            await setDoc(doc(db, "users", res.user.uid), {
+                uid: res.user.uid,
                 name,
                 email,
             });
+            toast.success("Successfull Registred")
         } catch (error) {
-
+            setErr(true)
+            toast.success("UnSuccessfull Registred")
         }
 
     }
@@ -74,14 +79,6 @@ function Registration() {
                                                                         <label className="form-label" for="form3Example4c">Password</label>
                                                                     </div>
                                                                 </div>
-
-                                                                {/* <div className="d-flex flex-row align-items-center mb-4">
-                                                                    <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                                                                    <div className="form-outline flex-fill mb-0">
-                                                                        <input type="password" id="form3Example4cd" className="form-control" />
-                                                                        <label className="form-label" for="form3Example4cd">Repeat your password</label>
-                                                                    </div>
-                                                                </div> */}
 
                                                                 <div className="form-check d-flex justify-content-center mb-3">
                                                                     <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
@@ -195,77 +192,3 @@ function Registration() {
 }
 
 export default Registration
-{/* <section className="vh-100 " >
-<div className="container h-100">
-    <div className="row d-flex justify-content-center align-items-center h-100">
-        <div className="col-lg-12 col-xl-11">
-            <div className="card text-black rounded-3" >
-                <div className="card-body p-md-5">
-                    <div className="row justify-content-center">
-                        <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-
-                            <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-
-                            <form className="mx-1 mx-md-4">
-
-                                <div className="d-flex flex-row align-items-center mb-4">
-                                    <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                                    <div className="form-outline flex-fill mb-0">
-                                        <input type="text" id="form3Example1c" className="form-control" />
-                                        <label className="form-label" for="form3Example1c">Your Name</label>
-                                    </div>
-                                </div>
-
-                                <div className="d-flex flex-row align-items-center mb-4">
-                                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                    <div className="form-outline flex-fill mb-0">
-                                        <input type="email" id="form3Example3c" className="form-control" />
-                                        <label className="form-label" for="form3Example3c">Your Email</label>
-                                    </div>
-                                </div>
-
-                                <div className="d-flex flex-row align-items-center mb-4">
-                                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                    <div class="form-outline flex-fill mb-0">
-                                        <input type="password" id="form3Example4c" class="form-control" />
-                                        <label class="form-label" for="form3Example4c">Password</label>
-                                    </div>
-                                </div>
-
-                                <div class="d-flex flex-row align-items-center mb-4">
-                                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                                    <div class="form-outline flex-fill mb-0">
-                                        <input type="password" id="form3Example4cd" class="form-control" />
-                                        <label class="form-label" for="form3Example4cd">Repeat your password</label>
-                                    </div>
-                                </div>
-
-                                <div class="form-check d-flex justify-content-center mb-5">
-                                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
-                                    <label class="form-check-label" for="form2Example3">
-                                        I agree all statements in <a href="#!">Terms of service</a>
-                                    </label>
-                                </div>
-
-                                <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                    <button type="button" class="btn btn-primary btn-lg">Register</button>
-                                </div>
-
-                            </form>
-
-                        </div>
-                        <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                                class="img-fluid" alt="Sample image" />
-
-                        </div>
-                    </div>
-                    <p class="text-center text-muted mt-5 mb-0">Have already an account? <Link href="/Login"
-                        class="fw-bold text-body"><u>Login here</u></Link></p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</section> */}
